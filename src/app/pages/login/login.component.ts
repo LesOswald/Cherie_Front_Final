d40429299ade0b492ea710e8afa373ba8d566133
Juan Mario Valderrama <juancho9402@hotmail.com>
2019-09-24 00:14:11 -0500

    Creating card form for Login component

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,11 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
+import { faSignInAlt, IconDefinition } from "@fortawesome/free-solid-svg-icons";
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+
+  faSignInAlt: IconDefinition = faSignInAlt;
 
   constructor() { }
 
--- src/app/pages/login/login.module.ts
+++ src/app/pages/login/login.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
+
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login.component';
 
@@ -9,7 +11,8 @@ import { LoginComponent } from './login.component';
   declarations: [LoginComponent],
   imports: [
     CommonModule,
-    LoginRoutingModule
+    LoginRoutingModule,
+    FontAwesomeModule
   ],
   exports: [LoginComponent]
 })
