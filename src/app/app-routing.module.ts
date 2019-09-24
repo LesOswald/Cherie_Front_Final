43022878b16e305a44fbe808b3c2a0b4e4cb7b2d
Juan Mario Valderrama <juancho9402@hotmail.com>
2019-09-23 21:20:11 -0500

    Lazy loading Login module on main routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'login',
+    loadChildren: () => import('./pages/login/login.module').then(mod => mod.LoginModule)
+  },
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'login'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
